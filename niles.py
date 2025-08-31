0dc9691f8da2c5c91921aafaa4d5497ee1f45fbc
mostafaasaad32 <mostafaasaad9878@gmail.com>
2025-08-31 17:34:13 +0300

    V1

--- niles.py
+++ niles.py
@@ -281,7 +281,7 @@ html, body, [data-testid="stAppViewContainer"], [data-testid="stMain"], .stApp {
 /* ====== EXTRA MOBILE LAYOUT FIXES ====== */
 @media (max-width: 768px) {
   body, .block-container, [data-testid="stAppViewContainer"] {
-    zoom: 1;
+    zoom: 0.95;
     -moz-transform: scale(1.2);
     -moz-transform-origin: 0 0;
   }
